d287bbcc99be63f411456b32228afe86ed6ef72f
DecafMegalodon <5885124+DecafMegalodon@users.noreply.github.com>
2020-12-01 16:30:30 -0500

    [D01a] The start of input

---
+++ day01a.cpp
@@ -0,0 +1,23 @@
+//https://adventofcode.com/2020/day/1
+//https://github.com/DecafMegalodon/AOC2020CPP
+
+#include <stdio.h>
+#include <vector>
+
+using namespace std;
+
+
+vector<int>* GetInput()
+{
+	int input;
+	int num_read;
+	vector<int>* input_vector = new vector<int>;
+	return input_vector;
+}
+
+int main(int argc, char** argv)
+{
+	vector<int>* input = GetInput();
+	
+	
+}
---
+++ tests/readme.md
@@ -0,0 +1,2 @@
+These tests are based on my c++ tester from my 2019 Advent of Code repository.
+If you're interested, you can find it at https://github.com/DecafMegalodon/AOC2019CPP in the form of cTest2.py
