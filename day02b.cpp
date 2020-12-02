1d82567b5b04d1de9eb5c52fdd8e4c7e7447e92e
DecafMegalodon <5885124+DecafMegalodon@users.noreply.github.com>
2020-12-02 01:21:39 -0500

    [D02b] Second verse, mostly same as the first

---
+++ day02b.cpp
@@ -0,0 +1,17 @@
+//https://adventofcode.com/2020/day/2
+//https://github.com/DecafMegalodon/AOC2020CPP
+
+#include "day02.hpp"
+#include <iostream>
+
+int main(int argc, char** argv){
+	int valid_passwords = 0;
+	vector<Password_Info*>* input = ReadInput();
+	
+	for(int i = 0; i < input->size(); ++i) {
+		Password_Info* cur = input->at(i);
+		valid_passwords += IsPasswordValid(cur);
+	}
+	
+	cout << valid_passwords << endl;
+}
