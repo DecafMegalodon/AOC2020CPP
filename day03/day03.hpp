17ca17ac55d8ab53b2bd60684605621f803cef44
DecafMegalodon <5885124+DecafMegalodon@users.noreply.github.com>
2020-12-03 09:46:17 -0500

    [D03] Let's get started

---
+++ day03/day03.hpp
@@ -0,0 +1,13 @@
+//https://adventofcode.com/2020/day/3
+//https://github.com/DecafMegalodon/AOC2020CPP
+
+#ifndef day_03_hpp_
+#define day_03_hpp_
+
+#include <vector>
+
+using namespace std;
+
+const int kSlopeWidth = 31;
+
+#endif
