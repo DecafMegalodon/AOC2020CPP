90e85b4b66999ad27de9260223d4af8db8f9c641
DecafMegalodon <5885124+DecafMegalodon@users.noreply.github.com>
2020-12-06 16:46:57 -0500

    [D05] Test cases and doc

--- day05/day05.hpp
+++ day05/day05.hpp
@@ -1,4 +1,11 @@
 //https://adventofcode.com/2020/day/5
 //https://github.com/DecafMegalodon/AOC2020CPP
 
+/*Takes a null-terminated character array `part_string`,
+an array of integers through which to binary partition,
+and the current `size` of `array` under consideration.
+Returns the indicated int in `array` OR the first int left in the range
+	if there's not enough partitions to fully specify
+A non 2^n size array should not be supplied. Behavior in such a case is undefined
+*/
 int PartSearch(char* part_string, int[] array, int size);
---
+++ tests/testcode/day05partition.cpp
@@ -0,0 +1,6 @@
+#include ../../day05.hpp
+
+int main(){
+	char sequence[] = "FBFBBFF";
+	
+}
