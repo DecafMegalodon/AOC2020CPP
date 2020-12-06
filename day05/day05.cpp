2027174a512c1f1929a5af208f8dcf080e0debcb
DecafMegalodon <5885124+DecafMegalodon@users.noreply.github.com>
2020-12-05 20:23:42 -0500

    A hopefully working binary search algorithm

--- day05/day05.cpp
+++ day05/day05.cpp
@@ -1,2 +1,16 @@
 //https://adventofcode.com/2020/day/5
 //https://github.com/DecafMegalodon/AOC2020CPP
+
+int PartSearch(char* part_string, int[] array, int size){
+	if(part_string[0] == '\n')
+		return array[0];
+		
+	switch(part_string[0]){
+		case 'F': // Keep the front half of the array
+		case 'L':
+			return PartSearch(part_string + 1, array, size / 2);
+		case 'B': //  Keep the back half of the array
+		case 'R':
+			return PartSearch(part_string + 1, array + (size / 2), size / 2);
+	}
+}
--- day05/day05.hpp
+++ day05/day05.hpp
@@ -1,2 +1,4 @@
 //https://adventofcode.com/2020/day/5
 //https://github.com/DecafMegalodon/AOC2020CPP
+
+int PartSearch(char* part_string, int[] array, int size);
