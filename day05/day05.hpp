ac68222a7132c7a8f230846bfbca8d159b6be0b3
DecafMegalodon <5885124+DecafMegalodon@users.noreply.github.com>
2020-12-06 16:52:19 -0500

    [D05] Refactor search to use a start:size range rather than an integer array

--- day05/day05.hpp
+++ day05/day05.hpp
@@ -2,10 +2,10 @@
 //https://github.com/DecafMegalodon/AOC2020CPP
 
 /*Takes a null-terminated character array `part_string`,
-an array of integers through which to binary partition,
-and the current `size` of `array` under consideration.
-Returns the indicated int in `array` OR the first int left in the range
-	if there's not enough partitions to fully specify
-A non 2^n size array should not be supplied. Behavior in such a case is undefined
+a `start` of consecutive integers through which to consider
+and the `size` of the run of intergers
+Returns the indicated int through partitioning or the first element in a range
+  if not enough partitions are supplied to fully describe and element
+A non 2^n size array is not supported. Behavior in such a case is undefined
 */
-int PartSearch(char* part_string, int[] array, int size);
+int PartSearch(char* part_string, int start, int size);
