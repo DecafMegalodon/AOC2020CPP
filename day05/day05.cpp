a170cab94d1c42ccf35122aac4363acac31bf448
DecafMegalodon <5885124+DecafMegalodon@users.noreply.github.com>
2020-12-06 17:28:06 -0500

    [D05] Binary partitioning and unit tests, new and improved

--- day05/day05.cpp
+++ day05/day05.cpp
@@ -1,8 +1,12 @@
 //https://adventofcode.com/2020/day/5
 //https://github.com/DecafMegalodon/AOC2020CPP
 
+#include <iostream>
+
+using namespace std;
+
 int PartSearch(char* part_string, int start, int size){
-	if(part_string[0] == '\n')
+	if(part_string[0] == '\0')
 		return start;
 		
 	switch(part_string[0]){
@@ -12,5 +16,9 @@ int PartSearch(char* part_string, int start, int size){
 		case 'B': //  Keep the back half of the array
 		case 'R':
 			return PartSearch(part_string + 1, start + (size / 2), size / 2);
+		default: {
+			cout << "Invalid partition supplied: " << part_string[0] << endl;
+			exit(-2);
+		}
 	}
 }
--- tests/testcode/day05partition.cpp
+++ tests/testcode/day05partition.cpp
@@ -1,6 +1,13 @@
-#include ../../day05.hpp
+#include <iostream>
+#include "../../day05/day05.hpp"
+
+using namespace std;
 
 int main(){
-	char sequence[] = "FBFBBFF";
-	
+	char* sequence = new char[20];
+	int start;
+	int size;
+	scanf("%s %d %d", sequence, &start, &size);
+	//int PartSearch(char* part_string, int start, int size){
+	cout << PartSearch(sequence, start, size) << endl;
 }
