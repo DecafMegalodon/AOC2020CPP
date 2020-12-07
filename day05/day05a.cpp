e8fd8791527d6602a189adcf5a1f7b4b67289664
DecafMegalodon <5885124+DecafMegalodon@users.noreply.github.com>
2020-12-07 10:23:52 -0500

    [D05a] Complete!

--- day05/day05a.cpp
+++ day05/day05a.cpp
@@ -2,10 +2,19 @@
 //https://github.com/DecafMegalodon/AOC2020CPP
 
 #include "day05.hpp"
+#include <iostream>  // cout
+#include <algorithm>  // max
 
 using namespace std;
 
 int main(){
-	vector<char*> inputs = GetInput();
+	vector<char*>* inputs = GetInput();
+	int cur_max_seatID = -1;
+	int cur_seatID;
 	
+	for(int i = 0; i < inputs->size(); i++){
+		cur_seatID = GetSeatID(inputs -> at(i));
+		cur_max_seatID = max(cur_max_seatID, cur_seatID);
+	}
+	cout << cur_max_seatID << endl;
 }
