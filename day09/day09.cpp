74c6faca2907cde9309bf46f1067f1c8c11284ac
DecafMegalodon <5885124+DecafMegalodon@users.noreply.github.com>
2020-12-09 08:27:05 -0500

    [D09] And the code, passing its test

--- day09/day09.cpp
+++ day09/day09.cpp
@@ -1,2 +1,16 @@
 //https://adventofcode.com/2020/day/8
 //https://github.com/DecafMegalodon/AOC2020CPP
+
+#include <iostream>
+#include <vector>
+#include "day09.hpp"
+
+using namespace std;
+
+vector<long>* GetInput(){
+	vector<long>* input = new vector<long>;
+	long data;
+	while(scanf("%ld\n", &data) != EOF)
+		input->push_back(data);
+	return input;
+}
--- day09/day09.hpp
+++ day09/day09.hpp
@@ -1,2 +1,11 @@
 //https://adventofcode.com/2020/day/8
 //https://github.com/DecafMegalodon/AOC2020CPP
+
+#ifndef day_09_hpp_
+#define day_09_hpp_
+
+#include <vector>
+
+std::vector<long>* GetInput();
+	
+#endif
