b1893317539e931ec93f6b53eed2a3ea9892ee19
DecafMegalodon <5885124+DecafMegalodon@users.noreply.github.com>
2020-12-09 09:18:00 -0500

    [D09] Unit test for summation testing

---
+++ tests/testcode/day09/day09check_sums.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+#include "../../../day09/day09.hpp"
+
+using namespace std;
+
+int main(){
+	vector<long>* input = GetInput();
+	for(int i = 5; i < input->size(); i++){
+		cout << Check_Sums(input->data(), 5, input->at(i)) << endl; 
+	}
+}
