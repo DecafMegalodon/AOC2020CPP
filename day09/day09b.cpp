2eea3bf89289bdb298915d4c2b670fb607ff3024
DecafMegalodon <5885124+DecafMegalodon@users.noreply.github.com>
2020-12-09 10:30:55 -0500

    [D09b] Starting

---
+++ day09/day09b.cpp
@@ -0,0 +1,16 @@
+//https://adventofcode.com/2020/day/8
+//https://github.com/DecafMegalodon/AOC2020CPP
+
+#include "day09.hpp"
+#include <vector>
+#include <iostream>
+
+using namespace std;
+
+int main(){
+	const int kPreamble = 25;
+	vector<long>* vect_input = GetInput();
+	long fault = FindFault(vect_input, kPreamble);
+	cout << fault << endl;
+	return 0;
+}
