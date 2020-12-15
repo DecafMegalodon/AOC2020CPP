6aa4146333fb902b38d03284a4388b8f3e9c3101
DecafMegalodon <5885124+DecafMegalodon@users.noreply.github.com>
2020-12-15 18:10:34 -0500

    [D04] Half-working input. Trying something else

--- day04/day04.cpp
+++ day04/day04.cpp
@@ -2,11 +2,29 @@
 //https://github.com/DecafMegalodon/AOC2020CPP
 
 #include <vector>
+#include <iostream>
 #include "day04.hpp"
+
+using namespace std;
 
 vector<Passport*>* GetInput(){
 	vector<Passport*>* input = new vector<Passport*>;
+	char key[4];
+	char data[41];
+	int read_result = 0;
 	
+	while(true){
+		read_result = scanf("%3s:%40s ", key, data);
+		cout << key << '@' << data << "RR:" << read_result << endl;
+		if(read_result == EOF)
+			break;
+		if(read_result != 2){
+			cout << "NEWPASSPORT!\n";
+		}
+		
+		
+		
+	}
 	
 	return input;
 }
