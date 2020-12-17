6309fbea4968dea591d386501e78cb2a08e8c1c7
DecafMegalodon <5885124+DecafMegalodon@users.noreply.github.com>
2020-12-17 11:20:21 -0500

    [D04] Add invisible space to end of test result lines to match driver

--- tests/testcode/day04/day04input.cpp
+++ tests/testcode/day04/day04input.cpp
@@ -8,7 +8,8 @@ using namespace std;
 int main(){
 	vector<Passport*>* input = GetInput();
 	for(auto passport: *input){  //For passport in input...
-		for(auto field: *passport->data_){
+		//cout << passport->data_->size() << endl;
+		for(auto field: *passport->data_){  //For data pair in the passport...
 			cout << field.first->c_str() << ':' << field.second->c_str() << ' ';
 		}
 		cout << endl;
